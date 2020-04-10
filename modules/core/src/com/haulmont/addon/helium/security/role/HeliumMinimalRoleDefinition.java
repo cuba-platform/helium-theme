28cc4764624beec00749ae8178290ba1ca85074d
Gleb Gorelov <gorelov@haulmont.com>
2020-04-10 11:55:53 +0400

    Add built-in role #6

---
+++ modules/core/src/com/haulmont/addon/helium/security/role/HeliumMinimalRoleDefinition.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2008-2020 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.haulmont.addon.helium.security.role;
+
+import com.haulmont.cuba.security.app.role.AnnotatedRoleDefinition;
+import com.haulmont.cuba.security.app.role.annotation.Role;
+import com.haulmont.cuba.security.app.role.annotation.ScreenAccess;
+import com.haulmont.cuba.security.role.ScreenPermissionsContainer;
+
+@Role(name = HeliumMinimalRoleDefinition.ROLE_NAME)
+public class HeliumMinimalRoleDefinition extends AnnotatedRoleDefinition {
+
+    public static final String ROLE_NAME = "helium-theme-minimal";
+
+    @ScreenAccess(screenIds = {"helium_ThemeSettingsScreen"})
+    @Override
+    public ScreenPermissionsContainer screenPermissions() {
+        return super.screenPermissions();
+    }
+
+    @Override
+    public String getLocName() {
+        return "Helium Theme Minimal";
+    }
+}
